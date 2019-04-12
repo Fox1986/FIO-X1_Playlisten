e84c619aeea537b9457e78d82a6c2f0c4f645afe
Fox1986 <Liquid-Fox@web.de>
2019-04-12 08:52:17 +0200

    Erste Änderungen für die Clementine-Playlisten erstellt. Pfade gesetzt, sodass das Programm nicht im selben ordner sein muss

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/**-----------------------------------------------------------------------------------------------#
+# Title:            FIOX1-Playlistenkonverter                                                   #
+# Description:      Passt .m3u-Playlisten an die Voraussetzungen des FIOX1 an                   #
+#                   Die Dateipfade mÃ¼ssen in der Playlist angepasst werden.                     #
+# Author:           Hinrik Taeger                                                               #
+# Probs:            -                                                                           #
+# Version:          1.1                                                                         #
+# Category:         Music                                                                       #
+# Tested System:    Linux Mint 19.1                                                             #
+# Date:             13.03.2018                                                                  #
+#-----------------------------------------------------------------------------------------------**/
+
+
 #include <iostream>
 #include <fstream>
 #include <dirent.h>
@@ -27,12 +40,12 @@ int main()
         string titel =  "Dieses Programm passt die FOOBAR2000-Playlists dem FIIOX1-Format an";
         headline(titel);
 
-        //zeige alle verfügbaren Dateien in dem Ordner. Das soll die Eingabe erleichtern.
+        //zeige alle verfÃ¼gbaren Playlisten im Ordner. Das soll die Eingabe erleichtern.
         cout << "Vorhandene Playlists: " << endl;
         DIR *hdir;
         struct dirent *entry;
 
-        hdir = opendir(".");
+        hdir = opendir("/home/fox/Musik/MP3/00_Wiedergabelisten"); //Hier den Pfad zu den unkonvertierten Playlisten einstragen
         do {
             entry = readdir(hdir);
             if (entry){
@@ -48,15 +61,19 @@ int main()
 
         cout << "Bitte zu konvertierende m3u-Datei eingeben (ohne Dateityp): ";
         cin >> playlist;
-        // Es wird eine zweite Playlist erstellt, damit die alte für FOOBAR2000 erhalten bleibt und weiter genutzt werden kann.
-        input.open(playlist+".m3u", ios::in);
-        output.open(playlist+"_FX1.m3u", ios::out|ios::app);
+        // Es wird eine zweite Playlist erstellt, damit die alte fÃ¼r FOOBAR2000 erhalten bleibt und weiter genutzt werden kann.
+        input.open("/home/fox/Musik/MP3/00_Wiedergabelisten/" + playlist+".m3u", ios::in);
+        output.open("/home/fox/Musik/MP3/00_FIOX1_Skoda/" + playlist+"_FX1.m3u", ios::out|ios::app);
         while (!input.eof()){
             getline(input, reader);
-            reader.erase(0,21); // Findet den allgemeinen Pfad bis zu den Genre-Ordnern
-            reader.erase(0,reader.find("\\"));   //Löscht den Genre-Ordner, sodass nur noch Interpreten etc. übrig bleiben
-            reader.insert(0,".."); //Dadurch ist es Möglich die Playlists in einem eigenen Ordner zu lagern.
-            output << reader << "\n";
+            if (reader[0]=='/'){
+                reader.erase(0,19); // Findet den allgemeinen Pfad bis zu den Genre-Ordnern
+                //reader.erase(0,reader.find("\\"));   //Lï¿½scht den Genre-Ordner, sodass nur noch Interpreten etc. ï¿½brig bleiben
+                reader.insert(0,".."); //Dadurch ist es Mï¿½glich die Playlists in einem eigenen Ordner zu lagern.
+                output << reader << "\n";
+            }else{
+                output << reader << "\n";
+            }
         }
         cout << "Playlist in " << playlist << "_FX1.m3u konvertiert!" << endl;
         input.close();
